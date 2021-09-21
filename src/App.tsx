d9ca0f3077ac4373966bc0f577ff08bbf837e88f
sofimmendoza@gmail.com <sofimmendoza@gmail.com>
2021-09-21 19:13:23 -0300

    realizamos el componente de inhabitants

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Inhabitant} from './data/inhabitants';
 import logo from './logo.svg';
 import './App.css';
 
---
+++ src/components/Inhabitants/index.tsx
@@ -0,0 +1,15 @@
+import { FC } from "react";
+import {Inhabitant as InhabitantType} from '../../data/inhabitants';
+
+type Props = {
+    data:InhabitantType;
+};
+
+const Inhabitant: FC<Props> = (this.props.) => {
+    
+    return
+    
+
+};
+
+export {Inhabitant}; 
